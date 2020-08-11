128d90df29d6720856a17e372fed7d7ca8edd1d3
wangmc01 <wangmc01@mingyuanyun.com>
2020-08-11 12:28:37 +0800

    新增登录路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import AppView from '@/components/app-view'
+import Login from '@/views/login'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       // name: 'HelloWorld',
       component: AppView
-    }
+    },
+    {
+      path: '/login',
+      component: Login
+    },
   ]
 })
